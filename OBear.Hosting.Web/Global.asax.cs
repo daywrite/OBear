32ec544679db0b5e0ea37ef15a93f3cb67e243d9
daywrite@yeah.net <daywrite@yeah.net>
2014-09-29 17:33:31 +0800

    菜单读取

--- OBear.Hosting.Web/Global.asax.cs
+++ OBear.Hosting.Web/Global.asax.cs
@@ -7,6 +7,9 @@ using System.Web.Optimization;
 using System.Web.Routing;
 
 using OBear.Plugin.Mvc;
+
+using UIShell.NavigationService;
+
 namespace OBear.Hosting.Web
 {
     public class MvcApplication : BundleRuntimeMvcApplication
@@ -18,9 +21,27 @@ namespace OBear.Hosting.Web
         //RouteConfig.RegisterRoutes(RouteTable.Routes);
         //BundleConfig.RegisterBundles(BundleTable.Bundles);
         //}
+        //菜单项
+        public static IEnumerable<NavigationNode> NavigationNodes { get; set; }
+
         protected override void Application_Start(object sender, EventArgs e)
         {
             base.Application_Start(sender, e);
+
+            //给菜单赋值
+            NavigationNodes = NavigationInitialize();
+
+        }
+
+        //通过插件获取到菜单项
+        private IEnumerable<NavigationNode> NavigationInitialize()
+        {
+            INavigationService service = BundleRuntime.GetFirstOrDefaultService<INavigationService>();
+            if (service == null)
+            {
+                return new List<NavigationNode>();
+            }
+            return service.NavgationNodes;
         }
 
         public override void RegisterResourceBundles()
--- OBear.Hosting.Web/Plugins/OBear.Plugin.Demo/BundleActivator.cs
+++ OBear.Hosting.Web/Plugins/OBear.Plugin.Demo/BundleActivator.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Web;
 
 using UIShell.OSGi;
+using UIShell.PageFlowService;
 
 namespace OBear.Plugin.Demo
 {
     public class BundleActivator : IBundleActivator
     {
+        public static IBundle Bundle { get; private set; }
+
+        public static ServiceTracker<IPageFlowService> PageFlowServiceTracker { get; set; }        
+
+        //public static PageNode LayoutPageNode
+        //{
+        //    get { return PageFlowServiceTracker.DefaultOrFirstService.GetPageNode("LayoutPage"); }
+        //}
+
         public void Start(IBundleContext context)
         {
-            
+            Bundle = context.Bundle;
+            PageFlowServiceTracker = new ServiceTracker<IPageFlowService>(context);
+
         }
 
         public void Stop(IBundleContext context)
