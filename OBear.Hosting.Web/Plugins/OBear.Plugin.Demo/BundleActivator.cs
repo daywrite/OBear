73072f97ebe828d7684e65b1181171033d824bf0
daywrite@yeah.net <daywrite@yeah.net>
2014-09-29 14:31:11 +0800

    第一个插件

---
+++ OBear.Hosting.Web/Plugins/OBear.Plugin.Demo/BundleActivator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using UIShell.OSGi;
+
+namespace OBear.Plugin.Demo
+{
+    public class BundleActivator : IBundleActivator
+    {
+        public void Start(IBundleContext context)
+        {
+            
+        }
+
+        public void Stop(IBundleContext context)
+        {
+        
+        }
+    }
+}
