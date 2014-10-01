30809e76b4c3d7daea43aea848f2fd2a205676e8
daywrite@yeah.net <daywrite@yeah.net>
2014-10-01 23:29:49 +0800

    菜单展示

---
+++ OBear.Hosting.Web/Plugins/OBear.Plugin.Admin/BundleActivator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using UIShell.OSGi;
+using UIShell.PageFlowService;
+
+namespace OBear.Plugin.Admin
+{
+    public class BundleActivator : IBundleActivator
+    {
+        public static IBundle Bundle { get; private set; }
+
+        public static ServiceTracker<IPageFlowService> PageFlowServiceTracker { get; private set; }
+
+        public void Start(IBundleContext context)
+        {
+            Bundle = context.Bundle;
+            PageFlowServiceTracker = new ServiceTracker<IPageFlowService>(context);
+        }
+
+        public void Stop(IBundleContext context)
+        {
+        
+        }
+    }
+}
